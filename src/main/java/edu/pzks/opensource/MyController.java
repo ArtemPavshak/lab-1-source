0eb8b54bcbf7ec57a71c2b602615f17c6991bc2e
gprohorov <g.prokhorov@indevlab.com>
2025-03-05 11:56:12 +0200

    sandbox - master

---
+++ src/main/java/edu/pzks/opensource/MyController.java
@@ -0,0 +1,38 @@
+package edu.pzks.opensource;
+
+
+/*
+  @author   george
+  @project   open-source
+  @class  MyController
+  @version  1.0.0 
+  @since 26.02.25 - 12.36
+*/
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello")
+public class MyController {
+
+    @GetMapping("/user")
+    public String helloUser() {
+        return " <h1>hello user<h1>";
+    }
+
+    @GetMapping("/admin")
+    public String helloAdmin() {
+        return "hello admin";
+    }
+
+    @GetMapping("/unknown")
+    public String helloUnknown() {
+        return "hello unknown";
+    }
+
+
+
+
+}
